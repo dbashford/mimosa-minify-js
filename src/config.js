24fe350a75dcab1aa66d6b7fa28349b3c309d5e5
Andrew Tayman <dretay@gmail.com>
2014-08-04 11:28:18 -0400

    added ability to toggle uglify mangle names

--- src/config.js
+++ src/config.js
@@ -3,7 +3,8 @@
 exports.defaults = function() {
   return {
     minifyJS: {
-      exclude:[/\.min\./]
+      exclude:[/\.min\./],
+      mangleNames: true
     }
   };
 };
@@ -12,11 +13,13 @@ exports.defaults = function() {
 exports.placeholder = function () {
   var ph = "\n\n  minifyJS:                     # Configuration for minifying/cleaning js using the\n" +
      "                                # --minify flag\n" +
-     "    exclude:[/\\.min\\./]         # List of string paths and regexes to match files to exclude\n" +
+     "    exclude:[/\\.min\\./]       # List of string paths and regexes to match files to exclude\n" +
      "                                # when running minification. Any path with \".min.\" in its name,\n" +
      "                                # is assumed to already be minified and is ignored by default.\n" +
      "                                # Paths can be relative to the watch.compiledDir, or absolute. \n" +
-     "                                # Paths are to compiled files,  so '.js' rather than '.coffee'\n\n";
+     "                                # Paths are to compiled files,  so '.js' rather than '.coffee'\n\n" +
+     "    mangleNames:true            # Controls whether or not UglifyJS is allowed to reduce names of\n " +
+     "                                # local variables and functions to single letters";
 
   return ph;
 };
@@ -26,5 +29,6 @@ exports.validate = function( config, validators )  {
   if ( validators.ifExistsIsObject( errors, "minifyCSS config", config.minifyJS ) ) {
     validators.ifExistsFileExcludeWithRegexAndString( errors, "minifyJS.exclude", config.minifyJS, config.watch.compiledDir );
   }
+  validators.ifExistsIsBoolean(errors, "minifyJS.mangleNames", config.minifyJS.mangleNames);
   return errors;
 };
